e219df24aef902c4b0d35dacd98ab1a0463f61a1
Prathamesh Warankar <prathameshwarankar@gmail.com>
2025-06-14 19:38:12 +0530

    Db changes and init

---
+++ AceBookApp/Migrations/20250614132729_init-with-new-db.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AceBookApp.Migrations
+{
+    public partial class initwithnewdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
